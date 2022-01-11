149367a87f33930144de7742e1d3b68162361803
Anahit Khechumyan <Khechumyan63@gmail.com>
2022-01-11 23:23:39 +0400

    pizza

---
+++ src/main/java/com/example/demo/service/impl/TableServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.repository.TableRepository;
+import com.example.demo.service.TableService;
+
+public class TableServiceImpl implements TableService {
+
+
+
+
+}
